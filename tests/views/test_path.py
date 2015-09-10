e23393176eea852b141002bb747761e8896cd48f
A.P. Rajshekhar <randalap@redhat.com>
2015-09-10 17:49:41 +0530

    CP-254: Support in Crane for v2 registry support. flake8 changes

--- tests/views/test_path.py
+++ tests/views/test_path.py
@@ -12,28 +12,28 @@ class TestPath(base.BaseCraneAPITest):
         response = self.test_client.get('/v2/redhat/foo/test')
 
         self.assertEqual(response.status_code, 302)
-        self.assertTrue(response.headers['Content-Type'].startswith('text/html'))        
+        self.assertTrue(response.headers['Content-Type'].startswith('text/html'))
         self.assertTrue('foo/bar/images/test' in response.headers['Location'])
-    
+
     def test_valid_repo_name_without_trailing_slash(self):
         response = self.test_client.get('/v2/redhat/foo/test')
 
         self.assertEqual(response.status_code, 302)
-        self.assertTrue(response.headers['Content-Type'].startswith('text/html'))        
+        self.assertTrue(response.headers['Content-Type'].startswith('text/html'))
         self.assertTrue('/bar/images' in response.headers['Location'])
-        
+
     def test_repo_name_without_username(self):
         response = self.test_client.get('/v2/foo/test')
 
         self.assertEqual(response.status_code, 404)
         self.assertTrue(response.headers['Content-Type'].startswith('text/html'))
-        
+
     def test_repo_name_without_repo(self):
         response = self.test_client.get('/v2/redhat/test')
 
         self.assertEqual(response.status_code, 404)
         self.assertTrue(response.headers['Content-Type'].startswith('text/html'))
-        
+
     def test_repo_name_without_path(self):
         response = self.test_client.get('/v2/redhat/foo')
 
